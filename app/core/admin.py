da91b37f99e4ae7a4370c2e81e768c4d4679398e
Arsh Kalani <akalani2000@gmail.com>
2022-08-13 16:04:50 +0530

    Django Admin Customization

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin # noqa
+# Django Admin customization
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUseradmin
+
+from core import models
+
+
+class UserAdmin(BaseUseradmin):
+    # Define the admin pages for users.
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
